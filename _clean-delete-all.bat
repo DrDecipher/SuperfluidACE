5663ffe1a382aa09fddeb4f66e40411ee7abe1d4
TharynMSG <tharyn.valavanis@msg.com>
2025-06-09 06:40:24 -0700

    Fixed Clean and Delete bat

--- _clean-delete-all.bat
+++ _clean-delete-all.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Clean & delete all changes, including ignored files
 echo.
-set /p CONFIRM=CLEAN & DELETE; ARE YOU SURE? (type YES to continue): 
+set /p CONFIRM="CLEAN & DELETE; ARE YOU SURE? (type YES to continue): "
 if /I not "%CONFIRM%"=="YES" (
     echo.
     echo Aborted by user. Press any key to exit.
